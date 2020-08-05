bd02c7be06628c2ad0739bbfb895c1e148decedd
sajadkh <sajadkh@partdp.ir>
2020-08-05 16:43:03 +0430

    Initialize project

---
+++ src/test/java/com/stuportal/usermanager/UsermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuportal.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
